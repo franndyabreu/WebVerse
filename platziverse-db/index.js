625c5a1c4a46db83275803d10c4072b84fc9b80d
Franndy Abreu <franndyabreu@gmail.com>
2019-05-29 14:31:25 -0400

    Added a Test module for the Agent and Metric Models :sweat_smile:

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,30 +1,42 @@
 /* A Simple IoT Platform - DB Module */
-"use strict";
+'use strict'
 
-const setupDB = require("./lib/db");
-const setupMetricModel = require("./models/metric");
-const setupAgentModel = require("./models/agent");
+const setupDB = require('./lib/db')
+const setupMetricModel = require('./models/metric')
+const setupAgentModel = require('./models/agent')
+const defaults = require('defaults')
 
-module.exports = async function(config) {
-  const sequelize = setupDB(config);
-  const AgentModel = setupAgentModel(config);
-  const MetricModel = setupMetricModel(config);
+module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+  const sequelize = setupDB(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
 
-  AgentModel.hasMany(MetricModel);
-  MetricModel.belongsTo(AgentModel);
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
 
-  await sequelize.authenticate();
+  await sequelize.authenticate()
 
-  /* CAUTION : The property force if set to true will overwrite 
+  /* CAUTION : The property 'force' if set to true will overwrite
           the database if it already exists. */
   if (config.setup) {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: true })
   }
-  const Agent = {};
-  const Metric = {};
+  const Agent = {}
+  const Metric = {}
 
   return {
     Agent,
     Metric
-  };
-};
+  }
+}
--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,41 +1,42 @@
-"use strict";
+'use strict'
 
-const db = require("./");
-const debug = require("debug")("Platzi-Verse:db:setup");
-const inquirer = require("inquirer");
-const chalk = require("chalk");
-const prompt = inquirer.createPromptModule();
+const db = require('./')
+const debug = require('debug')('Platzi-Verse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const prompt = inquirer.createPromptModule()
 
-async function setup() {
+async function setup () {
   const answer = await prompt([
     {
-      type: "confirm",
-      name: "setup",
-      message: "By saying YES the database will be DELETED, are you sure?"
+      type: 'confirm',
+      name: 'setup',
+      message: 'By saying YES the database will be DELETED, are you sure?'
     }
-  ]);
+  ])
 
   if (!answer.setup) {
-    console.log("Nothing happened!");
-    return;
+    console.log('Nothing happened!')
+    return
   }
   const config = {
-    database: process.env.DB_NAME || "platziverse",
-    username: process.env.DB_USERNAME || "franndy",
-    password: process.env.DB_PASSWORD || "franndy",
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USERNAME || 'franndy',
+    password: process.env.DB_PASSWORD || 'franndy',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
     logging: s => debug(s),
     setup: true
-  };
-
-  function handleFatalError(err) {
-    console.log(`An error ocurred: ${err}`);
-    console.log(err.stack);
-    process.exit(1);
   }
-  await db(config).catch(handleFatalError);
-  console.log("Database Created");
+
+  function handleFatalError (err) {
+    console.log(`${chalk.red(`[FATAL ERROR]: ${err}`)}`)
+    console.log(err.stack)
+    process.exit(1)
+  }
+  await db(config).catch(handleFatalError)
+  console.log('Database Created')
+  process.exit(1)
 }
 
-setup();
+setup()
